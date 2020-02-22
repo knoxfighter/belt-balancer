6f52d47304cae890a261f45691c09a12319fefe6
knoxfighter <knoxfighter@asdff45.de>
2020-02-22 17:45:05 +0100

    made changes for v1.1.8

---
+++ migrations/2020-02-22_Belt-balancer_1.1.8.lua
@@ -0,0 +1,16 @@
+-- reload all balancers
+
+-- clear global tables
+global.belt_balancer_max_id = 0
+global.new_balancers = nil
+global.new_balancers = {}
+global.events = nil
+global.events = {}
+
+-- rebuild global tables with all existing balancers
+for _, surface in pairs(game.surfaces) do
+    local all_balancer = surface.find_entities_filtered { name = { "belt-balancer" } }
+    for i = 1, #all_balancer do
+        created_entity({ entity = all_balancer[i]})
+    end
+end
